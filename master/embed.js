4eb2dcf32ea0fab04f9c777bc891d573a4323c90
fredthedoggy <45927799+fredthedoggy@users.noreply.github.com>
2020-09-09 18:25:31 -0400

    Create embed.js

---
+++ master/embed.js
@@ -0,0 +1,10 @@
+exports.EmbedGen = function (recembed) {
+    const sentembed = recembed;
+    sentembed.color = 16096256;
+    sentembed.footer = {
+        "icon_url": "https://cdn.discordapp.com/avatars/547890787682222081/a_0b01c9b2ce041dfbf887ffdf4e664ace.gif?size=128",
+        "text": "MultiMusic by Fredthedoggy"
+    };
+    sentembed.timestamp = new Date()
+    return sentembed;
+};
